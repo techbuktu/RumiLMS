7129826884e508844df809dfc9f5b8526de698c6
Muhammad Jalloh <dev@TechBuktu.com>
2019-02-18 16:37:33 -0500

    Setup initial app-wide routing system in app/routing/app-routing.module

--- lms/static/lms/rumiLMS/src/app/modules/routing/app-routing.module.ts
+++ lms/static/lms/rumiLMS/src/app/modules/routing/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+//Import all components from /app/components for use in routing requests
+import { HomeComponent } from '../../components/home/home.component';
+import { StudentListComponent } from '../../components/student-list/student-list.component';
+import { StudentComponent } from '../../components/student/student.component';
+import { ClassListComponent } from '../../components/class-list/class-list.component';
+import { ClassComponent } from '../../components/class/class.component';
+
+const routes: Routes = [
+	{
+	   path: '',
+           component: HomeComponent
+	},
+
+	{
+	   path: 'students',
+	   component: StudentListComponent
+	 },
+
+	{
+	   path: 'students/:studentUrl',
+	   component: StudentComponent
+	},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
