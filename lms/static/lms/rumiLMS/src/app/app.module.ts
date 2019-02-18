3d5e1b49b834bf017626f5a7c67c3d1bf5f0bbbf
Muhammad Jalloh <dev@TechBuktu.com>
2019-02-18 01:23:15 -0500

    Generated the initial StudentList and Student detail components

--- lms/static/lms/rumiLMS/src/app/app.module.ts
+++ lms/static/lms/rumiLMS/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { StudentComponent } from './components/student/student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    StudentListComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
