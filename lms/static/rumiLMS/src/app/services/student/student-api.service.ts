1ff9357ba0728e9f134bd6c11d0a9ee0f5b285a5
Muhammad Jalloh <dev@TechBuktu.com>
2019-02-19 16:20:52 -0500

    Created StudentApiService

--- lms/static/rumiLMS/src/app/services/student/student-api.service.ts
+++ lms/static/rumiLMS/src/app/services/student/student-api.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentApiService {
+  ApiUrl: string = 'localhost:8000/api/students/';
 
-  constructor() { }
+  constructor(private Http: HttpClient) { 
+
+  }
+
+  getAllStudents(){
+    return this.Http.get(this.ApiUrl);
+  }
+
+  getStudentbyUrl(student_url){
+    return this.Http.get(this.ApiUrl + student_url);
+  }
+  
+  addStudent(student_data) {
+    return this.Http.post(this.ApiUrl, student_data);
+  }
+
+  updateStudent(student_url, updated_student_data){
+    return this.Http.put(this.ApiUrl + student_url, updated_student_data);
+  }
+
+  deleteStudent(student_url){
+    return this.Http.delete(this.ApiUrl + student_url);
+  }
+
 }
