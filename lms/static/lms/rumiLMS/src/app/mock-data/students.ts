7b75fc68aa93ae1199bd3525d446dcbac4d1f3b9
Muhammad Jalloh <dev@TechBuktu.com>
2019-02-18 21:50:20 -0500

    Created initial list of STUDENTS mock data

--- lms/static/lms/rumiLMS/src/app/mock-data/students.ts
+++ lms/static/lms/rumiLMS/src/app/mock-data/students.ts
@@ -1,8 +1,28 @@
 export const STUDENT_LIST = [
-{ },
-{ },
-{ },
-{ },
-{ },
+{
+    id: 1,
+    first_name: 'Muhammad',
+    last_name: 'Jalloh'
+ },
+{ 
+    id: 2,
+    first_name: 'Avia',
+    last_name: 'Weinstein'
+},
+{ 
+    id: 3,
+    first_name: 'John',
+    last_name: 'Doe'
+},
+{ 
+    id: 4,
+    first_name: 'Jane',
+    last_name: 'Smith',
+},
+{ 
+    id: 5,
+    first_name: 'Ali',
+    last_name: 'Rumi'
+},
 
 ]
