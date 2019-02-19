3f5c435f59cd47fe065729e36034305f6b025223
Muhammad Jalloh <dev@TechBuktu.com>
2019-02-19 09:28:28 -0500

    Added djangorestframework to settings.INSTALLED_APPS; setup lms.urls

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
 
 # Create your views here.
+def home(request):
+    """
+    Placeholder home page.
+    """
+    return HttpResponse("RumiLMS: We are here to revolutionized EdTech forever. Join us! :)")
