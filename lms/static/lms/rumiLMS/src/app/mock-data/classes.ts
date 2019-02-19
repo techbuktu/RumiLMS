97790f91d807a3ac4aaa9ba10a2e04d1bab63965
Muhammad Jalloh <dev@TechBuktu.com>
2019-02-18 21:45:10 -0500

    Created initial array of Class objects in mock-data/classes.ts

--- lms/static/lms/rumiLMS/src/app/mock-data/classes.ts
+++ lms/static/lms/rumiLMS/src/app/mock-data/classes.ts
@@ -1,5 +1,17 @@
 export const CLASS_LIST = [
-{ },
-{ },
-{ }
+{
+    id : 1
+    name: 'Python',
+    desc: 'This class takes the student on a tour of the Python programming language and its intricate details',
+ },
+ {
+    id : 2
+    name: 'JavaScript',
+    desc: 'This class is an indepth coverage of JavaScript for both backend and frontend development',
+ },
+ {
+    id : 3,
+    name: 'Mobile',
+    desc: 'This class is an introduction to native mobile development using NativeScript and TypeScript.',
+ },
 ]
