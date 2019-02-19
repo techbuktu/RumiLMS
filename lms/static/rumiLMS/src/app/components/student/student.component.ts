55a6411142a40979e508b444ccf51f97bcd8d18b
Muhammad Jalloh <dev@TechBuktu.com>
2019-02-19 17:48:56 -0500

    StudentDetailComponent update

--- lms/static/rumiLMS/src/app/components/student/student.component.ts
+++ lms/static/rumiLMS/src/app/components/student/student.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentApiService } from '../../services/student/student-api.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-student',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-
-  constructor() { }
+  studentUrl:string;
+  student_error_message: string;
+  student:any;
+  student_class_list:any;
+  constructor(private studentService:StudentApiService, private router:Router, private route: ActivatedRoute) { 
+    this.route.params.subscribe(
+      params => {
+        this.studentUrl = params.studentUrl;
+      },
+      err => {
+        this.student_error_message = "This Student does not exist."
+      },
+      () => {}
+    )
+  }
 
   ngOnInit() {
+    this.getStudentData();
+    //this.getClassesforStudent();
   }
+
+
+  getStudentData(){
+    this.studentService.getStudentbyUrl(this.studentUrl).subscribe(
+      data => {
+        this.student = data;
+        this.student_class_list = this.student['classes'];
+      },
+      err => {},
+      () => { }
+    )
+  }
+
+  //getClassesforStudent(){}
 
 }
