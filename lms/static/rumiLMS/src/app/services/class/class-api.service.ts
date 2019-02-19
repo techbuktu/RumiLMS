b59108447031a2711d8c49ff2f93bf26fbe7f17a
Muhammad Jalloh <dev@TechBuktu.com>
2019-02-19 16:34:07 -0500

    Implemented the ClassApiService

--- lms/static/rumiLMS/src/app/services/class/class-api.service.ts
+++ lms/static/rumiLMS/src/app/services/class/class-api.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClassApiService {
+  ApiUrl:string = 'localhost:8000/api/classes';
 
-  constructor() { }
+  constructor( private Http: HttpClient) { }
+
+  getAllClasses(){
+    return this.Http.get(this.ApiUrl);
+  }
+
+  getClassbyUrl(class_url){
+    return this.Http.get(this.ApiUrl + class_url);
+  }
+
+  addNewClass(class_data){
+    return this.Http.post(this.ApiUrl, class_data);
+  }
+
+  updateClass(class_url, updated_class_data){
+    return this.Http.put(this.ApiUrl + class_url, updated_class_data);
+  }
+
+  deleteClass(class_url){
+    return this.Http.delete(this.ApiUrl + class_url);
+  }
+
 }
