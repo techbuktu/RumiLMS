d968c71773cddbebf35e37fec88a2827fa30cd75
Muhammad Jalloh <dev@TechBuktu.com>
2019-02-19 14:57:56 -0500

    Create API serializers and views for the lms.[Student, Class] models

--- lms/api/serializers.py
+++ lms/api/serializers.py
@@ -1,5 +1,6 @@
-from lms.models import (Student, Class)
+from lms.models import Class, Student 
 from rest_framework import serializers
+
 
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
     """
@@ -8,8 +9,23 @@ class StudentSerializer(serializers.HyperlinkedModelSerializer):
     classes = serializers.HyperlinkedRelatedField(
         queryset = Class.objects.all(),
         view_name = 'lms_api:class_detail',
-        lookup_field = 'link'
+        lookup_field = 'link',
+        many=True
     )
     class Meta:
         model = Student 
         fields = ('first_name', 'last_name', 'link', 'classes')
+
+class ClassSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    A Serializer for the lms.Class model's API endpoint.
+    """
+    students = serializers.HyperlinkedRelatedField(
+        many=True,
+        queryset = Student.objects.all(),
+        lookup_field = 'link',
+        view_name = 'lms_api:student_detail'
+    )
+    class Meta:
+        model = Class 
+        fields = ('name', 'desc','link')
--- lms/api/views.py
+++ lms/api/views.py
@@ -1,6 +1,46 @@
 from lms.models import Student, Class 
 from lms.api.serializers import (StudentSerializer, ClassSerializer)
+from django.http import Http404
+from rest_framework import generics
+from rest_framework.reverse import reverse 
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view 
 
-
+#@api_view(['GET'])
 def lms_api_root():
     return "This is the API root with docs and links to main endpoints."
+
+
+class StudentListView(generics.ListCreateAPIView):
+    """
+    Serves the endpoint that either returns all lms.Student instances (if called via GET)
+    or creates a new lms.Student instance if called with POST.
+    """
+    queryset = Student.objects.all() 
+    serializer_class = StudentSerializer 
+    lookup_field = 'link'
+
+class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Returns a single matching lms.Student instance.
+    """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer 
+    lookup_field = 'link'
+
+class ClassListView(generics.ListCreateAPIView):
+    """
+    View to retrieve a single lms.Class instance when called 
+    through the matching API URL endpoint.
+    """
+    queryset = Class.objects.all()
+    serializer_class = ClassSerializer
+    lookup_field = 'link'
+
+class ClassDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Returns a single matching lms.Class instance.
+    """
+    queryset = Class.objects.all()
+    serializer_class = ClassSerializer
+    lookup_field = 'link'
