92ebdd28d2907e1a3bd5a1315c847cde7ecc5047
Muhammad Jalloh <dev@TechBuktu.com>
2019-02-21 08:49:09 -0500

    Generated an InterceptorsModule to handle HttpInterceptors for the app's API service requests

--- lms/api/views.py
+++ lms/api/views.py
@@ -5,6 +5,7 @@ from rest_framework import generics
 from rest_framework.reverse import reverse 
 from rest_framework.response import Response 
 from rest_framework.decorators import api_view 
+#from rest_framework.parsers import JSONParser 
 
 @api_view(['GET'])
 def lms_api_root(request, format=None):
