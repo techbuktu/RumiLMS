4a3b2027a8d7b860be01ca8534fc0866476e98bd
Muhammad Jalloh <dev@TechBuktu.com>
2019-02-23 16:00:36 -0500

    Added goToHome() app-wide navigate-to-homepage method in RumiNavComponent

--- lms/static/rumiLMS/src/app/modules/material/schematics/rumi-nav/rumi-nav.component.ts
+++ lms/static/rumiLMS/src/app/modules/material/schematics/rumi-nav/rumi-nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-rumi-nav',
@@ -15,6 +16,10 @@ export class RumiNavComponent {
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private router: Router) {}
 
+
+  goToHome(){
+    this.router.navigate(['/']);
+  }
 }
