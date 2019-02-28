970204c73ae07b48e99284adbcc806d32fee667e
Muhammad Jalloh <dev@TechBuktu.com>
2019-02-28 15:46:42 -0500

    Fixed router-Link-related error in RumiNavComponent.spec

--- lms/static/rumiLMS/src/app/modules/material/schematics/rumi-nav/rumi-nav.component.spec.ts
+++ lms/static/rumiLMS/src/app/modules/material/schematics/rumi-nav/rumi-nav.component.spec.ts
@@ -21,7 +21,7 @@ describe('RumiNavComponent', () => {
     TestBed.configureTestingModule({
       declarations: [RumiNavComponent],
       imports: [
-        //RouterTestingModule, //Uncomment after other fixes are done
+        RouterTestingModule, 
         NoopAnimationsModule,
         LayoutModule,
         MatButtonModule,
